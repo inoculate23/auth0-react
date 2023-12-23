461e701b744c92f078e9d173f6beec21d72390aa
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-23 07:53:58 -0800

    Update page-footer.js

--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -1,3 +1,4 @@
+@@ -0,0 +1,86 @@
 import React from "react";
 import { PageFooterHyperlink } from "./page-footer-hyperlink";
 
@@ -25,7 +26,7 @@ export const PageFooter = () => {
     <footer className="page-footer">
       <div className="page-footer-grid">
         <div className="page-footer-grid__info">
-        <div className="page-footer-info__message">
+          <div className="page-footer-info__message">
             <p className="page-footer-message__headline">
               <span>This sample application is brought to you by&nbsp;</span>
               <PageFooterHyperlink path="https://auth0.com/">
