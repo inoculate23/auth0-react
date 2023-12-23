2bf0fd7c1d2e9acf2cf0e81bdd6f80bcbf7f0f8b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-23 08:59:39 -0800

    Update hero-banner.js

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -1,26 +1,25 @@
 import React from "react";
 
 export const HeroBanner = () => {
-  const logo = "https://cdn.auth0.com/blog/developer-hub/react-logo.svg";
+  const logo = "https://github.com/inoculate23/auth0-react/blob/main/public/logo_lg.png?raw=true";
 
   return (
-    <div className="hero-banner hero-banner--pink-yellow">
+    <div className="hero-banner hero-banner--blue-aqua ">
       <div className="hero-banner__logo">
         <img className="hero-banner__image" src={logo} alt="React logo" />
       </div>
-      <h1 className="hero-banner__headline">Hello, React World!</h1>
+      <h1 className="hero-banner__headline">Haawke Neural Technology</h1>
       <p className="hero-banner__description">
-        This is a sample application that demonstrates the authentication flow
-        for React apps using <strong>Auth0</strong>.
+Advanced Ai and machine learning applications and integrations for business, data-science, musicians, and artists.
       </p>
       <a
         id="code-sample-link"
         target="_blank"
         rel="noopener noreferrer"
-        href="https://auth0.com/developers/hub/code-samples/spa/react-javascript/basic-authentication-with-react-router-6"
+        href="https://whisper-experimental.vercel.app/"
         className="button button--secondary"
       >
-        Check out the React code sample →
+        Check out Whisper. →
       </a>
     </div>
   );
