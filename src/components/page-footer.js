1fb9284566749a7ada420f36c3da699dbf668016
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-23 07:45:31 -0800

    Update page-footer.js

--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -25,23 +25,7 @@ export const PageFooter = () => {
     <footer className="page-footer">
       <div className="page-footer-grid">
         <div className="page-footer-grid__info">
-          <div className="page-footer-info__message">
-            <p className="page-footer-message__headline">
-              <span>This sample application is brought to you by&nbsp;</span>
-              <PageFooterHyperlink path="https://auth0.com/">
-                Auth0
-              </PageFooterHyperlink>
-            </p>
-            <p className="page-footer-message__description">
-              <PageFooterHyperlink path="https://auth0.com/docs/quickstarts/">
-                <span>
-                  Securely implement authentication using Auth0 on any stack and
-                  any device&nbsp;
-                </span>
-                <u>in less than 10 minutes</u>
-              </PageFooterHyperlink>
-            </p>
-          </div>
+       
           <div className="page-footer-info__button">
             <a
               id="create-account-button"
